3e0392dcb812e8a8cb82891496bc1b0f8f016aca
André Custódio <56931353+AndreZila01@users.noreply.github.com>
2023-10-25 17:42:31 +0100

    Update SQL.sql

--- TrackCar/SQL.sql
+++ TrackCar/SQL.sql
@@ -1,12 +1,41 @@
 Create database TrackCar;
 
+Create table tblConsumo{
+    idConsumo int identity(1,1) primary key not null, 
+    Consumo varchar(100) not null
+}
+
+create table tblCentroDeInspecao(
+    idCentro int identity(1,1) primary key not null,
+    NomeCentro varchar(150) not null,
+    Morada varchar(200) not null,
+    Telefone varchar(15) not null,
+    Email varchar(100) not null,
+    ChefeDoCentro varchar(100) not null,
+);
+
+Create table tblEstado(
+    idEstado int identity(1,1) primary key not null,
+    Estado varchar(50) not null,
+    MensagemdoEstado varchar(500) not null
+)
+
+Create table tblInspecoes(
+    idInspecao int identity(1,1) primary key not null,
+    DataHora datetime not null,
+    CentroDeInspecao int foreign key references tblCentroDeInspecao(idCentro),
+    idCarro int foreign key references tblCar(idCar),
+    idEstado int foreign key references tblEstado(idEstado),
+    Reprobado boolean not null,
+);
+
 Create table tblCar(
 	idCar int identity(1,1) primary key not null,
 	MatriculaCarro varchar(8) not null,
 	Modelo varchar(50) not null,
 	MesAno varchar(5) not null, -- AA/MM
 	CV int not null, --celindrada
-	Consumo varchar(50) not null, -- eletrico, gasoleo, gasolina, GPL, hibrido
+	Consumo int Foreign key references tblConsumo(idConsumo), -- eletrico, gasoleo, gasolina, GPL, hibrido
 	KmFeitos Decimal(25, 2) not null,
 	LastLocalization varchar(max),
 );
@@ -18,6 +47,27 @@ Create table tblLogin(
 	Icon Image,
 );
 
+Create table tblMetodoDePagament(
+    idMetodo int identity(1,1) primary key not null,
+    Metodo varchar(30) not null,
+)
+Create table tblMulta(
+    idMulta int identity(1,1) primary key not null,
+    ValorMulta decimal(9, 2) not null,
+    Descricao varchar(300) not null,
+    idMetodo int foreign key references tblMetodoDePagament(idMetodo)
+    Pago boolean not null,
+    DataHoraPagamento datetime,
+    DataHoraMultado datetime not null,
+);
+
+Create table tbldetalhesCarro(
+    idDetalhesCarro int identity(1,1) primary key not null,
+    idCar int foreign key references tblCar(idCar),
+    idClient int foreign key references tblClient(idClient),
+    idMulta int foreign key references tblMulta(idMulta),
+);
+
 Create table tblClient(
 	idClient int identity(1,1) primary key not null,
 	FirstName varchar(50) not null,
@@ -25,6 +75,6 @@ Create table tblClient(
 	idLogin int Foreign Key references tblLogin(idLogin),
 	DataNascimento date not null,
 	Email varchar(150) not null,
-	idCarros int Foreign key references tblCar(idCar),
+	idCarros int Foreign key references tbldetalhesCarro(idDetalhesCarro),
 	FiltroNoticias varchar(max),
 );
