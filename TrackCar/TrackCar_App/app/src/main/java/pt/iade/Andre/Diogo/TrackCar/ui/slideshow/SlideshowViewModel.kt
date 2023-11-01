039c4ad0c422b5eb7acba9c103f944c1a6030189
André Custódio <andrecustodiotablet@gmail.com>
2023-11-01 13:57:19 +0000

    Start a simple menu

---
+++ TrackCar/TrackCar_App/app/src/main/java/pt/iade/Andre/Diogo/TrackCar/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package pt.iade.Andre.Diogo.TrackCar.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
