c2ca1a105266537721f7f3d7798e45daf5c16934
André Custódio <andrecustodiotablet@gmail.com>
2023-11-06 11:38:30 +0000

    Criado o menu (navbar) com animações

--- CarTrackApp/app/src/main/java/pt/iade/andre/diogo/cartrackapp/MainActivity.java
+++ CarTrackApp/app/src/main/java/pt/iade/andre/diogo/cartrackapp/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,27 +21,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     DrawerLayout drawerLayout;
     Toolbar toolBar;
-=======
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity {
-
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolBar;
-    ActionBarDrawerToggle actionBarDrawerToggle;
->>>>>>> c3345ca (Atualizada o menu e adicionado icons de ambiente)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         background_doWork(); // função para incrementar todos os valores aqui!!
     }
 
@@ -84,39 +68,5 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
-=======
-        background_doWork();
-    }
-
-    public void background_doWork(){
-        drawerLayout = findViewById(R.id.DrawerLayout);
-        navigationView = findViewById(R.id.NavigationView);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.menu_open, R.string.menu_close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                /*switch (item.getItemId()){
-                    case R.id.nav_car:
-                        Log.i("MENU_DRAWER_TAG", "Home item is clicked");
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                        break;
-
-                }*/
-                return true;
-            }
-        });
-
-            /*
-            * <item android:id="@+id/nav_car" android:title="Carros" android:icon="@drawable/car"></item>
-    <item android:id="@+id/nav_schedule" android:title="Agendamento" android:icon="@drawable/schedule"></item>
-    <item android:id="@+id/nav_screwdriver" android:title="Inspeções" android:icon="@drawable/screwdriver"></item>
-    <item android:id="@+id/nav_wallet" android:title="Carteira" android:icon="@drawable/wallet"></item>
-            * */
->>>>>>> c3345ca (Atualizada o menu e adicionado icons de ambiente)
     }
 }
