c360e4ead198faeb61c333ce4aa02d8226335dbb
Diogo205 <11.diogo.nunes@gmail.com>
2023-12-03 13:37:03 +0000

    alteraçoes

--- CarTrackApp/app/src/main/java/pt/iade/andre/diogo/cartrackapp/CarrosActivity.java
+++ CarTrackApp/app/src/main/java/pt/iade/andre/diogo/cartrackapp/CarrosActivity.java
@@ -2,24 +2,44 @@ package pt.iade.andre.diogo.cartrackapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarrosActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle bundle){
+    protected void onCreate(Bundle savedInstanceState) {
         Toast.makeText(getApplicationContext(), "Clicado o 2", Toast.LENGTH_LONG).show();
-        super.onCreate(bundle);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_carros);
 
 
+        List<String> listaDeCarros = obterListaDeCarros();
+
+        ListView listViewCarros = findViewById(R.id.activity_lista_carros);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.row_lista_carros, R.id.textviewMarca1, listaDeCarros);
+
+        listViewCarros.setAdapter(adapter);
     }
 
-    /*
-    * https://cdn.dribbble.com/users/2366356/screenshots/6766094/dribbble_shot_4x.png
-    * */
+        private List<String> obterListaDeCarros() {
+        List<String> carros = new ArrayList<>();
+        carros.add("Carro 1");
+        carros.add("Carro 2");
+        carros.add("Carro 3");
 
+        return carros;
+        }
     }
+
+
 
